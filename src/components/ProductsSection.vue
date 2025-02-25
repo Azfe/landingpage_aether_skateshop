05919f0574bebefbe89a575c3a6e41f265a7863e
Alex Zapata <alexzapata1984@gmail.com>
2025-02-25 20:53:31 +0100

    Add ProductsSection: Structure and design of the first part of the section

--- src/components/ProductsSection.vue
+++ src/components/ProductsSection.vue
@@ -2,5 +2,30 @@
 </script>
 
 <template>
+    <!-- Hero -->
+    <section class="w-full h-full bg-white">
+        <div class="flex flex-col py-20 mx-30">
+            <h2 class="text-5xl text-primary text-center font-bold">Skateboards</h2>
+            <hr class="border-t-6 border-secondary mx-auto my-7 w-3/4 rounded-full">            
+            <p class="text-center text-tertiary text-base mt-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum tempus nibh, ultricies sodales magna dictum in. Ut porttitor eros in purus tincidunt auctor. Aliquam semper dolor at erat scelerisque pretium. Fusce lorem tortor, rhoncus quis enim faucibus, facilisis pellentesque metus. Donec a consequat orci, ut facilisis tellus. Mauris non tortor tellus. Donec urna quam, placerat vel quam sit amet, malesuada aliquam lacus.
+            </p>            
+            <div class="flex lg:w-1/2 pt-10 items-center mx-auto">
+                <img src="../assets/images/products/01_tarot_moon.jpg" alt="Tarot Moon">
+            </div> 
+            <div class="flex flex-col space-y-2 py-6 mx-auto items-center">
+                <h6 class="text-gray-75">Skate completo</h6>
+                <h4 class="text-primary">Tarot Moon Complete</h4>
+                <small class="text-gray-75">7.75" - 8.25"</small>                                
+            </div>
+            <button class="
+                bg-primary text-white border border-transparent rounded-md px-20 py-1 mx-auto
+                hover:bg-white hover:text-primary hover:border-primary transition duration-300"
+            >
+                Comprar
+            </button>  
+        </div>
+
+    </section>
     
 </template>
