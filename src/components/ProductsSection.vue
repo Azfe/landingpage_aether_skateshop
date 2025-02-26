e38d9e60c72f458f7077fe2dcb53d1e244fef173
Alex Zapata <alexzapata1984@gmail.com>
2025-02-26 15:53:43 +0100

    Modify ProductsSection: New Products, cards and slide

--- src/components/ProductsSection.vue
+++ src/components/ProductsSection.vue
@@ -28,28 +28,58 @@
     </section>
     <!-- New Products -->
     <section class="w-full h-full bg-gray-75">
-        <h2 class="text-5xl text-secondary font-bold">Novedades</h2>
+        <h2 class="text-5xl text-secondary font-bold pt-20 px-30">Novedades</h2>
         <div class="w-auto items-center mt-6 overflow-x-auto overscroll-x-contain flex space-x-4 lg:space-x-6 overflow-y-hidden">
             <!-- Cards -->
             <div class="Card">
-                <div class="w-full border border-secondary bg-transparent p-4 rounded-lg max-w-sm mx-auto">
-                    <img src="../assets/images/products/02_day_legs.jpg" alt="Day legs" class="w-full rounded-md mb-4">
+                <div class="w-full border border-secondary bg-transparent p-4 rounded-4xl max-w-sm mx-auto">
+                    <img src="../assets/images/products/02_day_legs.jpg" alt="Day legs" class="w-80 rounded-4xl mb-4 mx-auto cursor-pointer">
                     <div class="flex justify-between items-center mb-2">
-                        <h2 class="text-xl font-bold text-secondary">Título de la Card</h2>
-                        <button class="bg-green-500 text-white px-2 py-1 rounded-md text-sm">Botón</button>
+                        <h2 class="text-xl font-bold text-secondary">8" Day Legs Complete</h2>
+                        <button class="w-20 border border-secondary bg-transparent text-secondary px-2 py-1 rounded-full mr-5 cursor-pointer">110€</button>
                     </div>
-                    <p class="text-secondary">Este es un texto descriptivo que se encuentra debajo del título y del botón.</p>
-                </div>
-
-                <div class="w-full border border-secondary bg-transparent p-4 rounded-lg max-w-sm mx-auto">
-                    <img src="../assets/images/products/02_day_legs.jpg" alt="Day legs" class="w-full rounded-md mb-4">
-                    <div class="flex justify-between items-center mb-2">
-                        <h2 class="text-xl font-bold text-secondary">Título de la Card</h2>
-                        <button class="bg-green-500 text-white px-2 py-1 rounded-md text-sm">Botón</button>
-                    </div>
-                    <p class="text-secondary">Este es un texto descriptivo que se encuentra debajo del título y del botón.</p>
-                </div>
+                    <p class="text-secondary">Skate completo</p>
+                    <p class="text-secondary">Composición</p>
+                    <p class="text-secondary">[Tejido principal] 75% Madera de arce, 15% Acero, 8% Uretano, 2% Lija</p>
+                </div>                
             </div>
+            <div class="Card">
+                <div class="w-full border border-secondary bg-transparent p-4 rounded-4xl max-w-sm mx-auto">
+                    <img src="../assets/images/products/02_day_legs.jpg" alt="Day legs" class="w-80 rounded-4xl mb-4 mx-auto cursor-pointer">
+                    <div class="flex justify-between items-center mb-2">
+                        <h2 class="text-xl font-bold text-secondary">8" Day Legs Complete</h2>
+                        <button class="w-20 border border-secondary bg-transparent text-secondary px-2 py-1 rounded-full mr-5 cursor-pointer">110€</button>
+                    </div>
+                    <p class="text-secondary">Skate completo</p>
+                    <p class="text-secondary">Composición</p>
+                    <p class="text-secondary">[Tejido principal] 75% Madera de arce, 15% Acero, 8% Uretano, 2% Lija</p>
+                </div>                
+            </div>
+            <div class="Card">
+                <div class="w-full border border-secondary bg-transparent p-4 rounded-4xl max-w-sm mx-auto">
+                    <img src="../assets/images/products/02_day_legs.jpg" alt="Day legs" class="w-80 rounded-4xl mb-4 mx-auto">
+                    <div class="flex justify-between items-center mb-2">
+                        <h2 class="text-xl font-bold text-secondary">8" Day Legs Complete</h2>
+                        <button class="w-20 border border-secondary bg-transparent text-secondary px-2 py-1 rounded-full mr-5">110€</button>
+                    </div>
+                    <p class="text-secondary">Skate completo</p>
+                    <p class="text-secondary">Composición</p>
+                    <p class="text-secondary">[Tejido principal] 75% Madera de arce, 15% Acero, 8% Uretano, 2% Lija</p>
+                </div>                
+            </div>
+            <div class="Card">
+                <div class="w-full border border-secondary bg-transparent p-4 rounded-4xl max-w-sm mx-auto">
+                    <img src="../assets/images/products/02_day_legs.jpg" alt="Day legs" class="w-80 rounded-4xl mb-4 mx-auto">
+                    <div class="flex justify-between items-center mb-2">
+                        <h2 class="text-xl font-bold text-secondary">8" Day Legs Complete</h2>
+                        <button class="w-20 border border-secondary bg-transparent text-secondary px-2 py-1 rounded-full mr-5">110€</button>
+                    </div>
+                    <p class="text-secondary">Skate completo</p>
+                    <p class="text-secondary">Composición</p>
+                    <p class="text-secondary">[Tejido principal] 75% Madera de arce, 15% Acero, 8% Uretano, 2% Lija</p>
+                </div>                
+            </div>
+
         </div>
     </section>
     
