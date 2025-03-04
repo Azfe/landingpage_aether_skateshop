f8af4170bbc17e1fe53e3c59761adaf436f62b04
Alex Zapata <alexzapata1984@gmail.com>
2025-03-04 20:11:20 +0100

    Modify FooterSection: Add hover states and active colors to links

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -6,40 +6,72 @@
                 <h4 class="uppercase">Nuestra tienda</h4>
                 <hr class="border-t-1 border-white w-50 my-2 rounded-full"> 
                 <ul>
-                    <li>Devoluciones</li>
-                    <li>Política de privacidad</li>
-                    <li>Envíos</li>
-                    <li>Sobre nosotros</li>
-                    <li>Forma de pago</li>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Devoluciones</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Política de privacidad</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Envíos</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Sobre nosotros</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Forma de pago</li>
+                    </a>
                 </ul>
             </div>
             <div class="w-full md:w-1/2 lg:w-1/4 max-w-xs px-8">
                 <h4 class="uppercase">Productos</h4>
                 <hr class="border-t-1 border-white w-50 my-2 rounded-full">
                 <ul>
-                    <li>Los más vendidos</li>
-                    <li>Ofertas</li>
-                    <li>Novedades</li>                    
+                    <a href="#" class="text-white hover:text-primary active:text-secondary">
+                        <li>Los más vendidos</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Ofertas</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Novedades</li>
+                    </a>
                 </ul>
             </div>
             <div class="w-full md:w-1/2 lg:w-1/4 max-w-xs px-8">
                 <h4 class="uppercase">Cómo elegir el material</h4>
                 <hr class="border-t-1 border-white w-50 my-2 rounded-full"> 
                 <ul>
-                    <li>Qué skate completo elegir</li>
-                    <li>Skate para principiantes</li>
-                    <li>Cómo elegir un skate para bowl o rampa</li>                    
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Qué skate completo elegir</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Skate para principiantes</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Cómo elegir un skate para bowl o rampa</li>
+                    </a>
                 </ul>
             </div>
             <div class="w-full md:w-1/2 lg:w-1/4 max-w-xs px-8">
                 <h4 class="uppercase">Información de la tienda</h4>
                 <hr class="border-t-1 border-white w-50 my-2 rounded-full"> 
                 <ul>
-                    <li>Devoluciones</li>
-                    <li>Política de privacidad</li>
-                    <li>Envíos</li>
-                    <li>Sobre nosotros</li>
-                    <li>Forma de pago</li>
+                    <a href="#" class="text-white hover:text-primary active:text-secondary">
+                        <li>Devoluciones</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Política de privacidad</li>
+                    </a>
+                    <a href="" class="hover:text-primary active:text-secondary">
+                        <li>Envíos</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Sobre nosotros</li>
+                    </a>
+                    <a href="#" class="hover:text-primary active:text-secondary">
+                        <li>Forma de pago</li>
+                    </a>
                 </ul>
             </div>
         </div>
