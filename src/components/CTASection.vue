a937c34b78c5b851493087a23e3b4dd1fc971b84
Alex Zapata <alexzapata1984@gmail.com>
2025-03-05 00:13:37 +0100

    Modify CTASection: Add content + modify styles

--- src/components/CTASection.vue
+++ src/components/CTASection.vue
@@ -5,9 +5,12 @@
     <section class="w-full h-200 bg-youslide bg-cover bg-center">
         <div class="flex h-full items-end justify-center lg:justify-end text-white mx-30">
             <div class="flex flex-col space-y-8 py-10 md:items-center">
-                <div class="flex flex-col space-y-2 py-10 md:items-center">
-                    <h3 class="text-4xl font-bold text-tertiary">Deslízate con estilo</h3>
-                    <p class="text-lg">Nueva colección de ropa</p>
+                <h2 class="text-4xl font-bold text-white">Nueva colección</h2>
+                <p class="text-lg">ropa y complementos</p>
+            </div>
+            <div class="flex flex-col pt-10 pb-20 md:items-center">
+                <div class="flex flex-col py-5 md:items-center">
+                    <h3 class="text-4xl font-bold text-tertiary">Deslízate con estilo</h3>                  
                 </div>
                 <button class="
                     bg-primary text-white border border-transparent rounded-xl px-20 py-1 mx-auto
@@ -15,8 +18,7 @@
                 >
                     Patinar con estilo
                 </button>
-            </div>           
-            
+            </div>             
         </div>
     </section>    
 </template>
