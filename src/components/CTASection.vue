844d88a6111f74a75f8c638695ca8487345049d1
Alex Zapata <alexzapata1984@gmail.com>
2025-03-06 11:40:34 +0100

    CTASection: Repositioning elements with flex

--- src/components/CTASection.vue
+++ src/components/CTASection.vue
@@ -3,17 +3,17 @@
 
 <template>
     <section class="w-full h-200 bg-youslide bg-cover bg-center">
-        <div class="flex h-full items-end justify-center lg:justify-end text-white mx-30">
-            <div class="flex flex-col space-y-8 py-10 md:items-center">
+        <div class="flex flex-col h-full items-between justify-between text-white mx-30">
+            <div class="flex flex-col space-y-2 py-12 items-center lg:items-start">
                 <h2 class="text-4xl font-bold text-white">Nueva colección</h2>
-                <p class="text-lg">ropa y complementos</p>
-            </div>
-            <div class="flex flex-col pt-10 pb-20 md:items-center">
-                <div class="flex flex-col py-5 md:items-center">
+                <p class="text-xl">ropa y complementos</p>
+            </div>            
+            <div class="flex flex-col pt-10 pb-20 items-center lg:items-end">
+                <div class="flex flex-col py-5">
                     <h3 class="text-4xl font-bold text-tertiary">Deslízate con estilo</h3>                  
                 </div>
                 <button class="
-                    h-9 bg-primary text-white border border-transparent rounded-xl px-20 py-1 mx-auto
+                    h-9 bg-primary text-white border border-transparent rounded-xl px-20 py-1 text-sm lg:text-base
                     hover:bg-white hover:text-primary hover:border-primary active:border-secondary active:text-secondary 
                     transition duration-300 cursor-pointer"
                 >
